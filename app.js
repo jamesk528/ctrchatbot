c6a9db13d4f608b981c0640cad2ac1e18e86a9b9
James J. Kim <375358@cds.ci.la.ca.us>
2017-05-01 09:00:06 -0700

    Figuring out Dialog

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appID: null,
+    appPassword: null
+    // appId: process.env.MICROSOFT_APP_ID,
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
@@ -24,11 +26,11 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 // Bots Dialogs
 //=========================================================
-
 var intents = new builder.IntentDialog();
 bot.dialog('/', intents);
 
-intents.matches(/^Purchasing Card/i, [
+
+intents.matches(/^Purchasing Card limit/i, [
     function(session) {
         builder.Prompts.text(session, "The limit is $1000 per transaction and $5,000 per month. Would you like to request an exemption? ")
     }, 
@@ -46,13 +48,14 @@ intents.matches(/^Travel Card/i, [
     }
 ]);
 
+
 // bot.dialog('/', [
 //   function (session) {
 //     builder.Prompts.text(session, 'Hi! What is your name?');
 //   },
 //   function(session, results) {
 //     session.send("Hello %s! Do you have a question?", results.response);
-//   },
+//   }, 
 //     function(session) {
 //     session.beginDialog('/questions');
 // }
@@ -61,7 +64,7 @@ intents.matches(/^Travel Card/i, [
 
 //DvD5Cv4jMys.cwA.cgk.6-Z0LRE6k0DZi3shEp7h3T5t4J4Rg_sXCP5qMAyIkUo
 
-server.get('/', restify.serveStatic({
- directory: "C:\ctrchatbot",
- default: '/index.html'
-}));
+// server.get('/', restify.serveStatic({
+//  directory: "C:\ctrchatbot",
+//  default: '/index.html'
+// }));
